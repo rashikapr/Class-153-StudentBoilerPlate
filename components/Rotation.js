7b04ddaea79d8fb0791fdf0cdd48308f90a8f40d
rashikapr <74289013+rashikapr@users.noreply.github.com>
2023-10-05 20:35:30 +0530

    Add files via upload

---
+++ components/Rotation.js
@@ -0,0 +1,55 @@
+//Terrain Rotation
+AFRAME.registerComponent("terrain-rotation-reader", {
+  schema: {
+    speedOfRotation: { type: "number", default: 0 },
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "ArrowRight") {
+        if (this.data.speedOfRotation < 0.1) {
+          this.data.speedOfRotation += 0.01;
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (this.data.speedOfRotation > -0.1) {
+          this.data.speedOfRotation -= 0.01;
+        }
+      }
+    });
+  },
+  tick: function () {
+    var mapRotation = this.el.getAttribute("rotation");
+
+    mapRotation.y += this.data.speedOfRotation;
+
+    this.el.setAttribute("rotation", {
+      x: mapRotation.x,
+      y: mapRotation.y,
+      z: mapRotation.z,
+    });
+  },
+});
+
+//Plane rotation component
+AFRAME.registerComponent("plane-rotation-reader", {
+  schema: {
+    speedOfRotation: { type: "number", default: 0 },
+    speedOfAscent: { type: "number", default: 0 }
+  },
+  init: function () {
+    window.addEventListener("keydown", (e) => {
+
+      //get the data from the attributes
+      this.data.speedOfRotation = this.el.getAttribute("rotation");      
+      this.data.speedOfAscent = this.el.getAttribute("position");
+
+      var planeRotation = this.data.speedOfRotation;      
+      var planePosition = this.data.speedOfAscent;
+
+      //control the attributes with the Arrow Keys
+      
+    });
+  }
+});
+
+
